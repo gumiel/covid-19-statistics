2db7fd818ae013464715c5c6040d9296412d0d1f
HENRY <prez.gumiel@gmail.com>
2020-04-15 15:44:01 -0400

    React en proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { Navbar, Nav, Collapse, Dropdown, Alert, Container, Row, Col } from "bootstrap-4-react";
 import './App.css';
+import LineGraph from "./LineGraph";
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar expand="lg" light bg="light">
+        <Navbar.Brand href="#">COVID-19</Navbar.Brand>
+        <Navbar.Toggler target="#navbarNav" />
+        <Collapse navbar id="navbarNav">
+          <Navbar.Nav>
+            <Nav.ItemLink href="#" active>
+              Home
+            </Nav.ItemLink>
+            <Nav.ItemLink href="#">Features</Nav.ItemLink>
+            <Nav.ItemLink href="#">Pricing</Nav.ItemLink>
+            <Nav.Item dropdown>
+              <Nav.Link dropdownToggle>Dropdown</Nav.Link>
+              <Dropdown.Menu>
+                <Dropdown.Item>Action</Dropdown.Item>
+                <Dropdown.Item>Another action</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Nav.Item>
+            <Nav.ItemLink disabled>Disabled</Nav.ItemLink>
+          </Navbar.Nav>
+        </Collapse>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <Alert primary>Primary Alert</Alert>
+            <Alert secondary>Secondary Alert</Alert>
+          </Col>
+          <Col>
+            <LineGraph></LineGraph>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
