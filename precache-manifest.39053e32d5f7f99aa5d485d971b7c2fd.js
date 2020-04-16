1ed206da1b52b869d1c92534e2b9ac90fb4a5a03
Henry Perez Gumiel <henry.perez@corcbb.local>
2020-04-15 21:40:47 -0400

    Updates

---
+++ precache-manifest.39053e32d5f7f99aa5d485d971b7c2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94adfeb24b83769d406bdf269245b3ab",
+    "url": "/covid-19-statistics/index.html"
+  },
+  {
+    "revision": "1f6a19e1cb0f913288e8",
+    "url": "/covid-19-statistics/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f271d1ff95a450a0658c",
+    "url": "/covid-19-statistics/static/js/2.4b25ed23.chunk.js"
+  },
+  {
+    "revision": "a8f7278d8628210ee14296765db220fc",
+    "url": "/covid-19-statistics/static/js/2.4b25ed23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6a19e1cb0f913288e8",
+    "url": "/covid-19-statistics/static/js/main.fd57db98.chunk.js"
+  },
+  {
+    "revision": "64cd8f5c778fd861819a",
+    "url": "/covid-19-statistics/static/js/runtime-main.6aae97b7.js"
+  }
+]);
